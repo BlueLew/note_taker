058991bf7c25ec761261c024f3c6879c6d76289a
Lewis Cruz <bluelew75@gmail.com>
2020-07-15 15:39:48 -0400

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+	def show
+		@notes = @user.notes
+	end
+
 	def new
 		@user = User.new
 	end
