ce458174586db79b7b29fd32e127797a928adae7
Lewis Cruz <bluelew75@gmail.com>
2020-07-15 15:55:50 -0400

    Clean up layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 	def update
 		if @user.update(user_params)
 			flash[:notice] = "Your account information was successfully updated."
-			redirect_to notes_path
+			redirect_to @user
 		else
 			render 'edit'
 		end
