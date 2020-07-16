0fd56e99f85d05cc41ed773dd55ffba31c172690
Lewis Cruz <bluelew75@gmail.com>
2020-07-15 22:53:55 -0400

    Added ability for user to delete account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	has_many :notes
+	has_many :notes, dependent: :destroy
 	before_save { self.email = email.downcase }
 	validates :username, presence: true,
 						uniqueness: { case_sensitive:false }, 
